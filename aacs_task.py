6a920a410f312306f68eb3fde5bd2c34576a1e01
mklarita <147615571+mklarita@users.noreply.github.com>
2023-11-15 16:16:04 +0100

    Update aacs_task.py

--- aacs_task.py
+++ aacs_task.py
@@ -1,6 +1,8 @@
 import os
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from PIL import Image
+import io
 
 # Generate a random key
 def generate_key():
@@ -14,19 +16,20 @@ def encrypt(key, plaintext):
     ciphertext = encryptor.update(plaintext) + encryptor.finalize()
     return (iv, ciphertext)
 
-# Compute cover set for a given subset of devices
-def compute_cover_set(subset, n):
-    cover_set = set()
-    for device in subset:
-        path = [device]
-        while device > 1:
-            device = device // 2
-            path.append(device)
-        cover_set.update(path)
-    return cover_set
+# Image to Bytes
+def image_to_bytes(image_path):
+    with Image.open(image_path) as img:
+        with io.BytesIO() as byte_arr:
+            img.save(byte_arr, format=img.format)
+            return byte_arr.getvalue()
+
+# Bytes to Image
+def bytes_to_image(byte_data, output_path):
+    with Image.open(io.BytesIO(byte_data)) as img:
+        img.save(output_path)
 
 # Encrypt content based on the revoked devices
-def encrypt_with_revocation(content, revoked_devices, total_devices):
+def encrypt_with_revocation(image_path, revoked_devices, total_devices):
     root_key = generate_key()
     content_key = generate_key()
 
@@ -41,15 +44,26 @@ def encrypt_with_revocation(content, revoked_devices, total_devices):
     keys_for_cover_set = {node: generate_key() for node in cover_set}
     encrypted_keys = [encrypt(keys_for_cover_set[node], content_key)[1] for node in cover_set]
 
+    # Convert the image to bytes and encrypt
+    image_bytes = image_to_bytes(image_path)
+    encrypted_image_iv, encrypted_image = encrypt(root_key, image_bytes)
+
     return {
+        "encrypted_content": encrypted_image,
+        "encrypted_content_iv": encrypted_image_iv,
         "encrypted_content_key": encrypted_content_key,
         "encrypted_keys_for_cover_set": encrypted_keys
     }
 
-# Example usage:
-content = b'This is the content to encrypt.'
+# Example usage
+image_path = 'path_to_your_image.jpg'  # Replace with your image path
+output_path = 'encrypted_image.jpg'
 revoked_devices = {3, 7}  # Devices to revoke
 total_devices = 8  # Total number of devices
 
-encrypted_data = encrypt_with_revocation(content, revoked_devices, total_devices)
-print(encrypted_data)
+encrypted_data = encrypt_with_revocation(image_path, revoked_devices, total_devices)
+
+# Save the encrypted image
+bytes_to_image(encrypted_data['encrypted_content'], output_path)
+
+print("Image encryption completed. Check the encrypted image.")
